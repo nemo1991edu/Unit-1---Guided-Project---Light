756d5b02643b39af16aea95c73233d3ee8114ef1
eduardo rivadeneira <eduardorivadeneira@eduardos-MBP.hitronhub.home>
2021-05-22 16:08:20 -0700

    first commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Light
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var lightOn = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+}
+
